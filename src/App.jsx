23da73bff749d3eb6e2574d172bb042820a88723
Ahmed Gadir <ahmed.gadir@gmail.com>
2025-03-15 15:11:02 +0000

    update

---
+++ src/App.jsx
@@ -0,0 +1,119 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div
+			style={{
+				textAlign: "center",
+				fontSize: "1.5rem",
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+				justifyContent: "center",
+				width: "100%",
+				gap: `1rem`,
+				height: "100vh",
+				backgroundColor: "lightgray",
+			}}
+		>
+			<div className="w-900">
+				وعليكم السلام الحمد لله كيف حالكم اليوم؟ You could say something like
+			</div>
+			<div className="w-900">
+				وعليكم السلام الحمد لله كيف حالكم اليوم؟ You could say something like
+			</div>
+			<div className="w-800">
+				وعليكم السلام الحمد لله كيف حالكم اليوم؟ You could say something like
+			</div>
+			<div className="w-700">
+				وعليكم السلام الحمد لله كيف حالكم اليوم؟ You could say something like
+			</div>
+			<div className="w-600">
+				وعليكم السلام الحمد لله كيف حالكم اليوم؟ You could say something like
+			</div>
+			<div className="w-500">
+				وعليكم السلام الحمد لله كيف حالكم اليوم؟ You could say something like
+			</div>
+			<div className="w-400">
+				وعليكم السلام الحمد لله كيف حالكم اليوم؟ You could say something like
+			</div>
+			<div className="w-300">
+				وعليكم السلام الحمد لله كيف حالكم اليوم؟ You could say something like
+			</div>
+			<div className="w-200">
+				وعليكم السلام الحمد لله كيف حالكم اليوم؟ You could say something like
+			</div>
+			<div className="w-100">
+				وعليكم السلام الحمد لله كيف حالكم اليووم؟ You could say something like
+			</div>
+
+			{/* <div className="sf-arabic w-900">
+				وعليكم السلام الحمد لله كيف حالكم اليوم؟
+			</div>
+			<div className="sf-arabic w-800">
+				وعليكم السلام الحمد لله كيف حالكم اليوم؟
+			</div>
+			<div className="sf-arabic w-700">
+				وعليكم السلام الحمد لله كيف حالكم اليوم؟
+			</div>
+			<div className="sf-arabic w-600">
+				وعليكم السلام الحمد لله كيف حالكم اليوم؟
+			</div>
+			<div className="sf-arabic w-500">
+				وعليكم السلام الحمد لله كيف حالكم اليوم؟
+			</div>
+			<div className="sf-arabic w-400">
+				وعليكم السلام الحمد لله كيف حالكم اليوم؟
+			</div>
+			<div className="sf-arabic w-300">
+				وعليكم السلام الحمد لله كيف حالكم اليوم؟
+			</div>
+			<div className="sf-arabic w-200">
+				وعليكم السلام الحمد لله كيف حالكم اليوم؟
+			</div>
+			<div className="sf-arabic w-100">
+				وعليكم السلام الحمد لله كيف حالكم اليووم؟
+			</div> */}
+			{/* <div className="sf-arabic w-900">Assalamu Alaykum How Are You Today?</div>
+			<div className="sf-arabic w-800">Assalamu Alaykum How Are You Today?</div>
+			<div className="sf-arabic w-700">Assalamu Alaykum How Are You Today?</div>
+			<div className="sf-arabic w-600">Assalamu Alaykum How Are You Today?</div>
+			<div className="sf-arabic w-500">Assalamu Alaykum How Are You Today?</div>
+			<div className="sf-arabic w-400">Assalamu Alaykum How Are You Today?</div>
+			<div className="sf-arabic w-300">Assalamu Alaykum How Are You Today?</div>
+			<div className="sf-arabic w-200">Assalamu Alaykum How Are You Today?</div>
+			<div className="sf-arabic w-100">Assalamu Alaykum How Are You Today?</div> */}
+			{/* <div className="sf-pro w-900">
+				وعليكم السلام الحمد لله كيف حالكم اليوم؟
+			</div>
+			<div className="sf-pro w-800">
+				وعليكم السلام الحمد لله كيف حالكم اليوم؟
+			</div>
+			<div className="sf-pro w-700">
+				وعليكم السلام الحمد لله كيف حالكم اليوم؟
+			</div>
+			<div className="sf-pro w-600">
+				وعليكم السلام الحمد لله كيف حالكم اليوم؟
+			</div>
+			<div className="sf-pro w-500">
+				وعليكم السلام الحمد لله كيف حالكم اليوم؟
+			</div>
+			<div className="sf-pro w-400">
+				وعليكم السلام الحمد لله كيف حالكم اليوم؟
+			</div>
+			<div className="sf-pro w-300">
+				وعليكم السلام الحمد لله كيف حالكم اليوم؟
+			</div>
+			<div className="sf-pro w-200">
+				وعليكم السلام الحمد لله كيف حالكم اليوم؟
+			</div>
+			<div className="sf-pro w-100">
+				وعليكم السلام الحمد لله كيف حالكم اليووم؟
+			</div> */}
+			{/* <div className="test2">وعليكم السلام الحمد لله كيف حالكم اليوم؟</div> */}
+			{/* <div className="test3">وعليكم السلام الحمد لله كيف حالكم اليوم؟</div> */}
+		</div>
+	);
+}
+
+export default App;
